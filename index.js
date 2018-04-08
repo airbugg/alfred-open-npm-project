46ba1d265ac0112420dd87fc1a66d958f4b15096
Eugene Lerman <eugenel@wix.com>
2018-04-08 22:25:16 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+"use strict";
+const path = require("path");
+const alfy = require("alfy");
+const fg = require("fast-glob");
+
+const BASE_DIR = "~/dev";
+
+async function getProjectDirs() {
+	const fileList = await fg(
+		["/Users/eugenel/dev/{personal,work}/**/package.json"],
+		{
+			ignore: ["**/node_modules", "**/test", "**/dist"],
+			followSymlinkedDirectories: false,
+			deep: 3
+		}
+	);
+
+	return fileList.map(file => ({
+		title: path.basename(path.dirname(file)),
+		subtitle: path.dirname(file),
+		arg: `${path.dirname(file)}`
+	}));
+}
+
+getProjectDirs().then(results => alfy.output(results));
---
+++ readme.md
@@ -0,0 +1,22 @@
+# alfred-open-npm-project [![Build Status](https://travis-ci.org/airbugg/alfred-open-npm-project.svg?branch=master)](https://travis-ci.org/airbugg/alfred-open-npm-project)
+
+> Quickly browse &amp; open npm projects in your favorite editor
+
+
+## Install
+
+```
+$ npm install --global alfred-open-npm-project
+```
+
+*Requires [Node.js](https://nodejs.org) 4+ and the Alfred [Powerpack](https://www.alfredapp.com/powerpack/).*
+
+
+## Usage
+
+In Alfred, type `open-npm-project`, <kbd>Enter</kbd>, and your query.
+
+
+## License
+
+MIT © [Eugene Lerman](http://github.com/airbugg)
