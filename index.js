3c71a97e55fd0e6f05f328f72e1ddc0ad6dc7613
Eugene Lerman <eugenel@wix.com>
2018-06-10 19:34:55 +0300

    feat: add caching, touch-up lint config

--- index.js
+++ index.js
@@ -4,20 +4,32 @@ const alfy = require('alfy')
 const fg = require('fast-glob')
 
 const BASE_DIR = '~/dev'
+const BASE_GLOB = ['/Users/eugenel/dev/{personal,work}/**/{package.json,.git}']
+const IGNORE_GLOB = ['**/node_modules', '**/test', '**/dist']
 
 async function getProjectDirs() {
-  const fileList = await fg(['/Users/eugenel/dev/{personal,work}/**/{package.json,.git}'], {
-    ignore: ['**/node_modules', '**/test', '**/dist'],
+  const cachedResults = alfy.cache.get('projectDirs')
+
+  if (cachedResults) {
+    return cachedResults
+  }
+
+  const fileList = await fg(BASE_GLOB, {
+    ignore: IGNORE_GLOB,
     followSymlinkedDirectories: false,
     deep: 3,
     onlyFiles: false,
   })
 
-  return fileList.map(file => ({
+  const formattedResults = fileList.map(file => ({
     title: path.basename(path.dirname(file)),
     subtitle: path.dirname(file),
     arg: `${path.dirname(file)}`,
   }))
+
+  alfy.cache.set('projectDirs', formattedResults)
+
+  return formattedResults
 }
 
 getProjectDirs().then(results => alfy.output(results))
